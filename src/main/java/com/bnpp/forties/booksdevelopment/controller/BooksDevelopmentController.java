ebe4bdc2777d114cf6eb31d6c9577ea7923e773b
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-10 12:57:45 -0700

    refactor: moved api string to sub class for more readability

--- src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
+++ src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
@@ -7,22 +7,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/booksdevelopment")
+@RequestMapping(value = BooksDevelopmentController.APIRoutes.BooksController.ROOT_API)
 @RequiredArgsConstructor
 public class BooksDevelopmentController {
 
-
     private final BooksDevelopmentService booksDevelopmentService;
 
-    @GetMapping("/getallbooks")
+    @RequestMapping(method = RequestMethod.GET, value = APIRoutes.BooksController.GET_ALL_BOOKS)
     public List<Book> getAllBooks() {
         return booksDevelopmentService.getAllBooks();
     }
 
+    public class APIRoutes {
+
+        public class BooksController {
+            public static final String ROOT_API = "/api/booksdevelopment";
+            public static final String GET_ALL_BOOKS = "/getallbooks";
+        }
+    }
 
 }
+
