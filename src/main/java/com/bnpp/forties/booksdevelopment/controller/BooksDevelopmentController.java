abc6c57ef49057b42c50a526beec3a418e33b278
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-10 13:23:38 -0700

    feat: added swagger documentation for bookdevelopment controller

--- src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
+++ src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
@@ -3,9 +3,10 @@ package com.bnpp.forties.booksdevelopment.controller;
 
 import com.bnpp.forties.booksdevelopment.model.Book;
 import com.bnpp.forties.booksdevelopment.service.BooksDevelopmentService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class BooksDevelopmentController {
 
     private final BooksDevelopmentService booksDevelopmentService;
 
+    @ApiOperation(value = "Get list of Books in the Store ", response = Iterable.class, tags = "getAllBooks")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!")})
     @RequestMapping(method = RequestMethod.GET, value = APIRoutes.BooksController.GET_ALL_BOOKS)
     public List<Book> getAllBooks() {
         return booksDevelopmentService.getAllBooks();
