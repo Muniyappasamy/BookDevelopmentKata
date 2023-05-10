95a70763106cdc4d378e033b4e8c16099936e88a
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-10 10:53:13 -0700

    Initial commit with pom.xml with basic dependencies,readme and spring boot initializer basic package structure

---
+++ src/main/java/com/bnpp/forties/booksdevelopment/BooksdevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.bnpp.forties.booksdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksdevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksdevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnpp/forties/booksdevelopment/BooksdevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpp.forties.booksdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksdevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
