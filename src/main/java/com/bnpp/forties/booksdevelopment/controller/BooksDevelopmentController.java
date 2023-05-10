40f8cfbe62f520be93c6d6fcbb03d5be08e3892a
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-10 12:30:54 -0700

    refactor: added lombok required arg constructor for code maintenance

--- src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
+++ src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
@@ -3,6 +3,7 @@ package com.bnpp.forties.booksdevelopment.controller;
 
 import com.bnpp.forties.booksdevelopment.model.Book;
 import com.bnpp.forties.booksdevelopment.service.BooksDevelopmentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/booksdevelopment")
+@RequiredArgsConstructor
 public class BooksDevelopmentController {
 
 
-    @Autowired
-    BooksDevelopmentService booksDevelopmentService;
+    private final BooksDevelopmentService booksDevelopmentService;
 
     @GetMapping("/getallbooks")
     public List<Book> getAllBooks() {
