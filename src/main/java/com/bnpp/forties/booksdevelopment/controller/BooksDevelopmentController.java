c3fa704f4caff473781a2afe8e6846ace4a29de3
Muniyappasamy M <muni.mostwanted@gmail.com>
2023-05-11 02:34:53 -0700

    feat: calculate discount price post rest api added in controller

--- src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
+++ src/main/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentController.java
@@ -2,15 +2,18 @@ package com.bnpp.forties.booksdevelopment.controller;
 
 
 import com.bnpp.forties.booksdevelopment.model.Book;
+import com.bnpp.forties.booksdevelopment.model.BookDto;
+import com.bnpp.forties.booksdevelopment.model.CartSummaryReportDto;
 import com.bnpp.forties.booksdevelopment.service.BooksDevelopmentService;
+import com.bnpp.forties.booksdevelopment.service.PriceSummationService;
 import com.bnpp.forties.booksdevelopment.service.impl.BooksDevelopmentServiceImpl;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +23,8 @@ import java.util.List;
 public class BooksDevelopmentController {
 
     private final BooksDevelopmentService booksDevelopmentService;
+
+    private final PriceSummationService priceSummationService;
 
     @ApiOperation(value = "Get list of Books in the Store ", response = Iterable.class, tags = "getAllBooks")
     @ApiResponses(value = {
@@ -31,11 +36,16 @@ public class BooksDevelopmentController {
         return booksDevelopmentService.getAllBooks();
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = APIRoutes.BooksController.CALCULATE_DISCOUNT_PRICE)
+    public CartSummaryReportDto calculateDiscountPrice(@RequestBody List<BookDto> listOfBooks) {
+        return priceSummationService.getcartSummaryReport(listOfBooks);
+    }
     public class APIRoutes {
 
         public class BooksController {
             public static final String ROOT_API = "/api/booksdevelopment";
             public static final String GET_ALL_BOOKS = "/getallbooks";
+            public static final String CALCULATE_DISCOUNT_PRICE = "/calculatediscountprice";
         }
     }
 
--- src/test/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentControllerTest.java
+++ src/test/java/com/bnpp/forties/booksdevelopment/controller/BooksDevelopmentControllerTest.java
@@ -1,16 +1,24 @@
 package com.bnpp.forties.booksdevelopment.controller;
 
+import com.bnpp.forties.booksdevelopment.model.BookDto;
 import com.bnpp.forties.booksdevelopment.service.impl.BooksDevelopmentServiceImpl;
+import com.bnpp.forties.booksdevelopment.service.impl.PriceSummationServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = BooksDevelopmentController.class)
@@ -25,6 +33,10 @@ class BooksDevelopmentControllerTest {
     @MockBean
     private BooksDevelopmentServiceImpl booksDevelopmentServiceImpl;
 
+    @MockBean
+    private PriceSummationServiceImpl priceSummationServiceImpl;
+
+
     @Test
     @DisplayName("BooksDevelopment controller instance should not be null")
     void booksDevelopmentControllershouldNotBeNull() {
@@ -36,5 +48,20 @@ class BooksDevelopmentControllerTest {
     void getBooks_Api_shouldReturn_StatusOK() throws Exception {
         mockMvc.perform(get("/api/booksdevelopment/getallbooks")).andExpect(status().isOk());
     }
+
+
+    @Test
+    @DisplayName(" API calculate discount price should return status OK")
+    void calculateDiscountPrice_Api_shouldReturn_StatusOK() throws Exception {
+        List<BookDto> listOfBooks = new ArrayList<BookDto>();
+        BookDto bookDto1 = new BookDto("Clean Code",2);
+        BookDto bookDto2 = new BookDto("The Clean Coder",1);
+
+        listOfBooks.add(bookDto1);
+        listOfBooks.add(bookDto2);
+        mockMvc.perform(post("/api/booksdevelopment/calculatediscountprice").contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(listOfBooks))).andExpect(status().isOk());
+    }
+
 
 }
